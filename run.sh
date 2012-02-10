c7ab6dd6ff7720043dea47fef1c812d8119061fd
Wahoo Lab <wahoolab@gmail.com>
2012-02-09 23:57:52 -0600

    2/9 23:57

--- run.sh
+++
@@ -1,8 +0,0 @@
-# Always automatically replace /tmp/wahoo if it has been removed for some reason.
-if [ ! -f /tmp/wahoo ]; then 
-   cp /home/lab/Dropbox/wahoo/tmp/lab-vm1/ksh /tmp/wahoo 
-   chmod 700 /tmp/wahoo
-fi
-
-/home/lab/Dropbox/wahoo/bin/.wahoo-check-events.sh 
-
